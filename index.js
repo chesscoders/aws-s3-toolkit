ff7540a468f7ba30618f0b531cd9b3161a618fda
victorocna <victorocnarescu@gmail.com>
2024-09-04 17:43:08 +0300

    Export endpoint details

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const download = require('./src/download');
+const endpointDetails = require('./src/endpoint-details');
 const remove = require('./src/remove');
 const uploadPrivate = require('./src/upload-private');
 const upload = require('./src/upload');
 
 module.exports = {
   download,
+  endpointDetails,
   remove,
   uploadPrivate,
   upload,
