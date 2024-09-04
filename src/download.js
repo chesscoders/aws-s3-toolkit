79cd78283f1cd6afbbf039692887ec32ac1b0cb9
victorocna <victorocnarescu@gmail.com>
2024-09-04 10:37:56 +0300

    Add jsdoc

--- src/download.js
+++ src/download.js
@@ -3,6 +3,13 @@ const streamToBuffer = require('./stream-to-buffer');
 const s3Client = require('./s3-client');
 const endpointDetails = require('./endpoint-details');
 
+/**
+ * Downloads a file from an S3 bucket.
+ *
+ * @param {string} document - The name of the document to download.
+ * @returns {Promise<Buffer>} - A promise that resolves to the buffer containing the file data.
+ * @throws {Error} - Throws an error if the download fails.
+ */
 const download = async (document) => {
   const { bucket, folder } = endpointDetails();
   const params = {
--- src/upload.js
+++ src/upload.js
@@ -1,8 +1,16 @@
-require('dotenv').config();
 const { PutObjectCommand } = require('@aws-sdk/client-s3');
 const s3Client = require('./s3-client');
 const endpointDetails = require('./endpoint-details');
 
+/**
+ * Uploads a file to an S3 bucket.
+ *
+ * @param {string} filename - The name of the file to upload.
+ * @param {Buffer|string|ReadableStream} data - The data of the file to upload.
+ * @param {string} - The access control list (ACL) for the file. Default is 'public-read'.
+ * @returns {Promise<Object>} - A promise that resolves to the response from the S3 service.
+ * @throws {Error} - Throws an error if the upload fails.
+ */
 const upload = async (filename, data, acl = 'public-read') => {
   const { bucket, folder } = endpointDetails();
   const params = {
